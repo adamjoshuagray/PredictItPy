04001a2ecf587ba501b03616fe03c847f978afee
Adam J. Gray <adam.joshua.gray@gmail.com>
2016-04-07 21:38:36 +0800

    Bumped version number up.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='predictitpy',
-      version='0.1',
+      version='0.1.1',
       description='A very light wrapper around the PredictIt.org market data api.',
       url='https://github.com/adamjoshuagray/predictitpy',
       author='Adam J. Gray',
